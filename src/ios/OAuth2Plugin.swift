df0eccd6ac7543f30df02216c8382cb07653cbe9
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-11-13 13:06:00 +0100

    initial version iOS

--- src/ios/OAuth2Plugin.swift
+++ src/ios/OAuth2Plugin.swift
@@ -1,8 +1,41 @@
 import Foundation
 
  @objc(OAuth2Plugin) class OAuth2Plugin : CDVPlugin {
-    func getGoogleDriveFiles(command: CDVInvokedUrlCommand) {
-        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
-        commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
+    var accountId: String?
+    
+    func add(command: CDVInvokedUrlCommand) {
+        let args = command.arguments[0] as [String: String]
+        
+        self.accountId = args["accountId"]
+        var scopes: [String] = []
+        if args["scopes"] != nil {
+            scopes = args["scopes"]!.componentsSeparatedByString(",")
+        }
+        let config = Config(base: args["base"]!,
+            authzEndpoint: args["authzEndpoint"]!,
+            redirectURL: args["redirectURL"]!,
+            accessTokenEndpoint: args["accessTokenEndpoint"]!,
+            clientId: args["clientId"]!,
+            refreshTokenEndpoint: args["refreshTokenEndpoint"],
+            clientSecret: args["clientSecret"],
+            revokeTokenEndpoint: args["revokeTokenEndpoint"],
+            scopes: scopes,
+            accountId: args["accountId"])
+        
+        
+        AccountManager.addAccount(config, moduleClass: OAuth2Module.self)
+    }
+    
+    func requestAccess(command: CDVInvokedUrlCommand) {
+        let module = AccountManager.getAccountByName(self.accountId!)
+        module?.requestAccess({ (response, error ) in
+            var commandResult:CDVPluginResult
+            if (error != nil) {
+                commandResult = CDVPluginResult(status: CDVCommandStatus_OK)
+            } else {
+                commandResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: error?.description)
+            }
+            self.commandDelegate.sendPluginResult(commandResult, callbackId: command.callbackId)
+        })
     }
 }
--- www/oauth2.js
+++ www/oauth2.js
@@ -1,11 +1,40 @@
-/*global cordova*/
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 var exec = require("cordova/exec");
 
-var OAuth2 = function(){};
+var OAuth2 = function () {};
 
-OAuth2.prototype.getGoogleDriveFiles = function (successCallback) {
-    cordova.exec(successCallback, null, "OAuth2Plugin", "getGoogleDriveFiles", []);
+OAuth2.prototype.add = function (settings) {
+  cordova.exec(null, null, 'OAuth2Plugin', 'add', [settings]);
 };
+
+OAuth2.prototype.requestAccess = function () {
+  var success, error;
+
+  return new Promise(function (resolve, reject) {
+    error = function (error) {
+      reject({ error: error });
+    };
+
+    success = function (result) {
+      resolve(result);
+    };
+
+    cordova.exec(success, error, 'OAuth2Plugin', 'requestAccess', []);
+  });
+}
 
 module.exports = new OAuth2();
