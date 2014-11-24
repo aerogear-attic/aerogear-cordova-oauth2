201473c2e4b710cba3fe8c64015744ebc95688d0
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-11-24 13:40:14 +0100

    removed jars dependencies and added aar

--- src/android/OAuth2Plugin.java
+++ src/android/OAuth2Plugin.java
@@ -17,12 +17,11 @@
 package org.jboss.aerogear.cordova.oauth2;
 
 import android.util.Log;
-import android.util.Pair;
 import org.apache.cordova.CallbackContext;
 import org.jboss.aerogear.android.Callback;
+import org.jboss.aerogear.android.authorization.AuthzModule;
 import org.jboss.aerogear.android.impl.authz.AuthorizationManager;
 import org.jboss.aerogear.android.impl.authz.oauth2.OAuth2AuthorizationConfiguration;
-import org.jboss.aerogear.android.impl.authz.oauth2.OAuth2AuthzModule;
 import org.jboss.aerogear.cordova.android.reflect.BasePlugin;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,30 +29,28 @@ import org.json.JSONObject;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
-import java.util.HashSet;
 
 /**
  * @author edewit@redhat.com
  */
 public class OAuth2Plugin extends BasePlugin {
   private static final String TAG = OAuth2Plugin.class.getSimpleName();
-  private OAuth2AuthzModule module;
+  private AuthzModule module;
 
   public boolean add(JSONObject data, CallbackContext callbackContext) throws JSONException, MalformedURLException {
     Log.d(TAG, "add account");
 
-    OAuth2AuthorizationConfiguration config = AuthorizationManager.config("name", OAuth2AuthorizationConfiguration.class);
-    config.setBaseURL(new URL(data.getString("base")));
-    config.setAccountId(data.getString("accountId"));
-    config.setAccessTokenEndpoint(data.getString("accessTokenEndpoint"));
-    config.setAuthzEndpoint(data.getString("authzEndpoint"));
-    config.setClientId(data.getString("clientId"));
-    config.setRefreshEndpoint(data.getString("refreshTokenEndpoint"));
-    config.setRedirectURL(data.getString("redirectURL"));
-    //config.setAdditionalAccessParams(data.getString("scopes").split(","))
-    config.setAdditionalAccessParams(new HashSet<Pair<String, String>>(Arrays.asList(new Pair<String, String>("openid", "email"))));
+    module = AuthorizationManager.config("name", OAuth2AuthorizationConfiguration.class)
+        .setBaseURL(new URL(data.getString("base")))
+        .setAccountId(data.getString("accountId"))
+        .setAccessTokenEndpoint(data.getString("accessTokenEndpoint"))
+        .setAuthzEndpoint(data.getString("authzEndpoint"))
+        .setClientId(data.getString("clientId"))
+        .setRefreshEndpoint(data.getString("refreshTokenEndpoint"))
+        .setRedirectURL(data.getString("redirectURL"))
+        .setScopes(Arrays.asList(data.getString("scopes").split(",")))
+        .asModule();
 
-    module = (OAuth2AuthzModule) config.asModule();
     callbackContext.success();
     return true;
   }
