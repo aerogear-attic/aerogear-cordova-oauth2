8c16fda9f5fb7e797b79126dde37783d5eef3e8a
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-11-25 16:24:00 +0100

    added js doc made it more inline with js authz api

--- www/oauth2.js
+++ www/oauth2.js
@@ -15,18 +15,75 @@
 
 var exec = require("cordova/exec");
 
+/**
+  The global oauth2 object is the entry point for all methods
+  @class
+  @returns {object} oauth2 - The oauth2 api
+*/
 var OAuth2 = function () {};
 
-OAuth2.prototype.add = function (settings) {
-  cordova.exec(null, null, 'OAuth2Plugin', 'add', [settings]);
+/**
+  The OAuth2 adapter is the default type used when creating a new authorization module.
+  This constructor is instantiated when the "add()" method is called
+  @status Experimental
+  @param {String} name - the name used to reference this particular authz module
+  @param {Object} settings={} - the settings to be passed to the adapter
+  @param {String} settings.clientId - the client id/ app Id of the protected service
+  @param {String} settings.redirectURL - the URL to redirect to
+  @param {String} settings.authEndpoint - the endpoint for authorization
+  @param {String} [settings.validationEndpoint] - the optional endpoint to validate your token.  Not in the Spec, but recommend for use with Google's API's
+  @param {String} settings.scopes - a space separated list of "scopes" or things you want to access
+  @returns {void}
+  @example
+  oauth2.add({
+      name: "coolThing",
+      settings: {
+          clientId: "12345",
+          redirectURL: "http://localhost:3000/redirector.html",
+          authEndpoint: "http://localhost:3000/v1/authz",
+          scopes: "userinfo coolstuff"
+      }
+  });
+ */
+OAuth2.prototype.add = function (object) {
+  this[object.name] = function() {
+    oauth2.requestAccess(object.name);
+  }
+  object.settings['accountId'] = object.name;
+  cordova.exec(null, null, 'OAuth2Plugin', 'add', [object.settings]);
 };
 
+/**
+  Request Access - If the client has no accessToken this will iniciate the oauth "dance", and return the accessToken. If an accessToken was already supplied this will be retuned immediately
+  @returns {Object} The ES6 promise (accessToken as a response parameter; if an error is returned)
+  @example
+  oauth2.add({
+      name: "coolThing",
+      settings: {
+          clientId: "12345",
+          redirectURL: "http://localhost:3000/redirector.html",
+          authEndpoint: "http://localhost:3000/v1/authz",
+          scopes: "userinfo coolstuff"
+      }
+  });
+  // Make the call.
+  authz.services.coolThing.requestAccess()
+      .then( function( accessToken ){
+          ...
+      })
+      .catch( function( error ) {
+          // an error happened
+      });
+  });
+ */
 OAuth2.prototype.requestAccess = function (accountId) {
   var success, error;
 
   return new Promise(function (resolve, reject) {
     error = function (error) {
-      reject({ error: error });
+      reject({
+        error: error
+      });
     };
 
     success = function (result) {
