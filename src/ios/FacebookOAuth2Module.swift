e9d91c87307036e6c4cfbd91f0e381e2807d47a0
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-11-27 15:15:14 +0100

    updated lib

--- src/ios/FacebookOAuth2Module.swift
+++ src/ios/FacebookOAuth2Module.swift
@@ -16,7 +16,6 @@
 */
 
 import Foundation
-//import AeroGearHttp
 
 /**
 An OAuth2Module subclass specific to 'Facebook' authorization
@@ -58,8 +57,7 @@ public class FacebookOAuth2Module: OAuth2Module {
                         expiredIn = array[index+1]
                     }
                 }
-                //println("access:\(accessToken!) expires:\(expiredIn!)")
-                self.oauth2Session.saveAccessToken(accessToken, refreshToken: nil, expiration: expiredIn)
+                self.oauth2Session.saveAccessToken(accessToken, refreshToken: nil, accessTokenExpiration: expiredIn, refreshTokenExpiration: nil)
                 completionHandler(accessToken, nil)
             }
         })
--- src/ios/OAuth2Session.swift
+++ src/ios/OAuth2Session.swift
@@ -22,42 +22,54 @@ The protocol that an OAuth2 Session modules must adhere to and represent storage
 public protocol OAuth2Session {
     
     /**
-    * The account id.
+    The account id.
     */
     var accountId: String {get}
     
     /**
-    * The access token which expires.
+    The access token which expires.
     */
     var accessToken: String? {get set}
     
     /**
-    * The access token's expiration date.
+    The access token's expiration date.
     */
     var accessTokenExpirationDate: NSDate? {get set}
     
     /**
-    * The refresh tokens. This toke does not expire and should be used to renew access token when expired.
+    The refresh token's expiration date.
+    */
+    var refreshTokenExpirationDate: NSDate? {get set}
+    
+    /**
+    The refresh tokens. This toke does not expire and should be used to renew access token when expired.
     */
     var refreshToken: String? {get set}
     
     /**
-    * Check validity of accessToken. return true if still valid, false when expired.
+    Check validity of accessToken. return true if still valid, false when expired.
     */
     func tokenIsNotExpired() -> Bool
     
+    
     /**
-    * Clears any tokens storage
+    Check validity of refreshToken. return true if still valid, false when expired.
+    */
+    func refreshTokenIsNotExpired() -> Bool
+    
+    /**
+    Clears any tokens storage
     */
     func saveAccessToken()
     
     /**
-    * Save tokens information. Saving tokens allow you to refresh accesstoken transparently for the user without prompting
-    * for grant access.
+    Save tokens information. Saving tokens allow you to refresh accesstoken transparently for the user without prompting
+    for grant access.
     
     :param: accessToken the access token
     :param: refreshToken  the refresh token
-    :param: expiration the expiration
+    :param: accessTokenExpiration the expiration for the access token
+    :param: refreshTokenExpiration the expiration for the refresh token
     */
-    func saveAccessToken(accessToken: String?, refreshToken: String?, expiration: String?)
+    func saveAccessToken(accessToken: String?, refreshToken: String?, accessTokenExpiration: String?, refreshTokenExpiration: String?)
 }
