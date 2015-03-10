4a9bb40c063a542808bd4706448dcafbb1b5c6aa
Erik Jan de Wit <erikjan.dewit@gmail.com>
2015-03-10 09:56:44 +0100

    also add the swift runtime

--- hooks/addSwiftOptions.js
+++ hooks/addSwiftOptions.js
@@ -10,6 +10,7 @@ module.exports = function (context) {
 	var swiftOptions = [""];
 	swiftOptions.push("IPHONEOS_DEPLOYMENT_TARGET = 7.0");
 	swiftOptions.push("SWIFT_OBJC_BRIDGING_HEADER = " + srcDir + "Bridging-Header.h");
+    swiftOptions.push("LD_RUNPATH_SEARCH_PATHS = $(inherited) @executable_path/Frameworks");
  
 	fs.appendFileSync(xcconfigPath, swiftOptions.join('\n'));
 }
